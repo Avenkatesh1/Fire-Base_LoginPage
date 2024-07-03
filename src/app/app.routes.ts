99ebc2a026e98b57c89da10906831734cdb935a9
A Venkatesh <avenkateshanand1998@gmail.com>
2024-07-03 10:53:57 +0530

    implementing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { R3TargetBinder } from '@angular/compiler';
+import { RegisterComponent } from './pages/register/register.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"login",
+        pathMatch:"full"
+    },
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"register",
+        component:RegisterComponent
+    },
+    {
+        path:"",
+        component:LayoutComponent,
+        children:[
+            {
+                path:"dashboard",
+                component:DashboardComponent
+            }
+        ]
+    }
+];
